83124df04b79e8de2d3b8c7c2b2d8886886cbd4a
hoangx1801 <94049972+hoangx1801@users.noreply.github.com>
2022-08-20 17:02:37 +0700

    Delete JS_CB directory

--- JS_CB/logger.js
+++
@@ -1,9 +0,0 @@
-export const TYPE_LOG = 'log';
-export const TYPE_WARN = 'warn';
-export const TYPE_ERROR = 'error';
-
-function logger(log, type="log") {
-    console[type](log);
-}
-
-//export default logger; //default duy nhất
